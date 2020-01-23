08b16dc0d8e19353fb7e92e42268e4c52547f9ed
cedmail <cmailleux@jahia.com>
2020-01-23 11:02:31 -0500

    BACKLOG-11709 : render menu completely from registry BACKLOG-12190 : build recursive treeview structure based on registry

--- src/javascript/Administration/Administration.jsx
+++ src/javascript/Administration/Administration.jsx
@@ -11,6 +11,7 @@ import SiteWeb from '@jahia/moonstone/dist/icons/SiteWeb';
 import constants from './Administration.constants';
 import {Route, Switch} from 'react-router';
 import AdminIframe from './AdministrationIframe';
+import SiteIframe from './SiteIframe';
 import {loadNamespace} from './Administration.loadNamespace';
 
 const AdministrationGroup = () => {
@@ -34,44 +35,48 @@ const Administration = () => {
     }
 
     const routes = [];
-    const routesServer = registry.find({
+    const dataServer = [];
+    const dataSites = [];
+    let createTreeStructureAndAggregateRoutes = function (currentLevelRoute, parent, registryTargetParent) {
+        currentLevelRoute.forEach(route => {
+            let treeEntry = {
+                id: route.label.toLowerCase().replace(' ', ''),
+                label: route.label,
+                isSelectable: route.isSelectable,
+                children: []
+            };
+            if (route.icon !== null) {
+                treeEntry.iconStart = route.icon;
+            }
+
+            if (route.childrenTarget !== null) {
+                createTreeStructureAndAggregateRoutes(registry.find({
+                    type: 'route',
+                    target: `${registryTargetParent}-${route.childrenTarget}`
+                }), treeEntry.children);
+            }
+
+            if (route.isSelectable) {
+                treeEntry.route = route.defaultPath;
+                routes.push(route);
+            }
+
+            parent.push(treeEntry);
+        });
+    };
+
+    createTreeStructureAndAggregateRoutes(registry.find({
         type: 'route',
         target: 'administration-server'
-    });
-    const routesServerSystemHealth = registry.find({
+    }), dataServer, 'administration-server');
+
+    createTreeStructureAndAggregateRoutes(registry.find({
         type: 'route',
-        target: 'administration-server-systemhealth'
-    });
-    routes.push(...routesServer);
-    routes.push(...routesServerSystemHealth);
-    console.log('Administration routes', routesServer);
-    let dataServer = [
-        {
-            id: 'aboutjahia',
-            label: 'About Jahia',
-            iconStart: <Info/>,
-            route: `${constants.DEFAULT_ROUTE}/aboutJahia`,
-            children: []
-        },
-        {
-            id: 'configuration',
-            label: 'Configuration',
-            isSelectable: false,
-            children: []
-        },
-        {
-            id: 'systemhealth',
-            label: 'System Health',
-            isSelectable: false,
-            children: [{
-                id: 'systeminfos',
-                label: 'System Infos',
-                iconStart: <Info/>,
-                route: `${constants.DEFAULT_ROUTE}/systemInfos`,
-                children: []
-            }]
-        }
-    ];
+        target: 'administration-sites'
+    }), dataSites, 'administration-sites');
+
+    console.log('Administration routes', routes);
+
     let getSelectedItems = function () {
         let selectedItems = [];
         let split = history.location.pathname.split('/');
@@ -81,12 +86,19 @@ const Administration = () => {
     };
 
     let getOpenedByDefault = function () {
+        let selectedItem = getSelectedItems()[0];
         if (dataServer.find(leaf => {
-            console.log('leaf', leaf, getSelectedItems());
-            return leaf.id === getSelectedItems()[0];
+            return leaf.id === selectedItem;
         }) !== undefined) {
             console.log('returning server');
             return 'server';
+        }
+
+        if (dataSites.find(leaf => {
+            return leaf.id === selectedItem;
+        }) !== undefined) {
+            console.log('returning server');
+            return 'sites';
         }
 
         return '';
@@ -103,7 +115,12 @@ const Administration = () => {
                                       openedItems={getSelectedItems()}
                                       onClick={elt => history.push(elt.route)}/>
                         </AccordionItem>
-                        <AccordionItem id="sites" label="Sites" icon={<SiteWeb/>}/>
+                        <AccordionItem id="sites" label="Sites" icon={<SiteWeb/>}>
+                            <TreeView data={dataSites}
+                                      selectedItems={getSelectedItems()}
+                                      openedItems={getSelectedItems()}
+                                      onClick={elt => history.push(elt.route.replace(':siteKey', window.contextJsParameters.siteKey))}/>
+                        </AccordionItem>
                     </Accordion>
                 </SecondaryNav>
             }
@@ -120,8 +137,11 @@ const Administration = () => {
 
 export const registerAdministration = () => {
     registerRoute(<Administration/>);
-    registerRouteLv2(<AdminIframe route="aboutJahia"/>, 'aboutJahia', null, 'About Jahia', <Info/>);
-    registerRouteLv2(<AdminIframe route="systemInfos"/>, 'systemInfos', 'systemhealth', 'System Infos', <Info/>);
+    registerRouteLv2('server', <AdminIframe route="aboutJahia"/>, 'aboutJahia', 'About Jahia', null);
+    registerRouteLv2('server', null, 'configuration', 'Configuration', null, '', 1, 'configuration', false);
+    registerRouteLv2('server', null, 'systemHealth', 'System Health', null, '', 2, 'systemhealth', false);
+    registerRouteLv2('server', <AdminIframe route="systemInfos"/>, 'systemInfos', 'System Infos', null, 'systemhealth');
+    registerRouteLv2('sites', <SiteIframe route="manageModules"/>, ':siteKey/manageModules', 'Modules', null);
     registry.add('administrationGroupItem', {
         type: 'bottomAdminGroup',
         target: ['nav-root-bottom:1'],
