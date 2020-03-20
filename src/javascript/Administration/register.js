fa32c76379dfc32981134e125d766af46d1cc74e
Thomas Draier <tdraier@jahia.com>
2020-03-20 12:31:42 +0100

    BACKLOG-12877 : Fixed error when not allowed to get server perms, fixed empty screen (#23)

--- src/javascript/Administration/register.js
+++ src/javascript/Administration/register.js
@@ -1,7 +1,6 @@
 import {registry} from '@jahia/ui-extender';
 import constants from './Administration.constants';
 import Administration from './Administration';
-import AdministrationEmpty from './Administration.empty';
 import AdministrationGroup from './AdministrationGroup';
 import React, {Suspense} from 'react';
 
@@ -11,13 +10,6 @@ export const registerAdministration = () => {
         path: `${constants.DEFAULT_ROUTE}*`, // Catch everything administration and let the app handle routing logic
         defaultPath: constants.DEFAULT_ROUTE,
         render: v => <Suspense fallback="loading ..."><Administration match={v.match}/></Suspense>
-    });
-    registry.add('adminRoute', 'administration-server', {
-        omitFromTree: true,
-        targets: ['administration-server:999'],
-        path: `${constants.DEFAULT_ROUTE}`,
-        defaultPath: constants.DEFAULT_ROUTE,
-        render: () => <AdministrationEmpty/>
     });
     registry.add('primary-nav-item', 'administrationGroupItem', {
         targets: ['nav-root-admin:1'],
