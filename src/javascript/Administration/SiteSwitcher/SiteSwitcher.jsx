ef9dfc90ab70b9c23b3def13705f619f25fad96b
cedmail <cmailleux@jahia.com>
2020-04-03 07:20:21 -0400

    BACKLOG-12914: add tests that knownSites is not empty

--- src/javascript/Administration/SiteSwitcher/SiteSwitcher.jsx
+++ src/javascript/Administration/SiteSwitcher/SiteSwitcher.jsx
@@ -28,7 +28,7 @@ const SiteSwitcher = () => {
     }
 
     // + 1 for system sites as it is not in the list of knownSites
-    if ((current.knownSitesList.length + 1) !== data.jcr.result.nodes.length) {
+    if ((current.knownSitesList.length > 0) && ((current.knownSitesList.length + 1) !== data.jcr.result.nodes.length)) {
         Promise.all([refetch({
             displayLanguage: 'en'
         })]);
