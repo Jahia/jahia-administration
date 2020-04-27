75fc814f913fa2d84c87b775b1abcd9760ba3b36
Cedric Mailleux <cmailleux@jahia.com>
2020-04-27 11:11:17 -0400

    BACKLOG-13191: Add defaultLanguage property and languages property in… (#32)
    * BACKLOG-13191: Add defaultLanguage property and languages property in query to build correct URLs for site or switch language in SiteSwitcher.jsx
    
    * Refetch with correct lang

--- src/javascript/Administration/SiteSwitcher/SiteSwitcher.jsx
+++ src/javascript/Administration/SiteSwitcher/SiteSwitcher.jsx
@@ -8,12 +8,17 @@ import styles from './SiteSwitcher.scss';
 import {useHistory, useLocation} from 'react-router-dom';
 
 const SiteSwitcher = () => {
+    const current = useSelector(state => ({
+        site: state.site,
+        knownSitesList: state.administration.sites,
+        uilang: state.uilang,
+        language: state.language
+    }));
     const {loading, refetch, data} = useQuery(SitesQuery, {
         variables: {
-            displayLanguage: 'en'
+            displayLanguage: current.uilang
         }
     });
-    const current = useSelector(state => ({site: state.site, knownSitesList: state.administration.sites}));
     const dispatch = useDispatch();
     const history = useHistory();
     const location = useLocation();
@@ -21,6 +26,9 @@ const SiteSwitcher = () => {
     function handleOnChange(e, item) {
         history.push(location.pathname.replace('/' + current.site + '/', '/' + item.name + '/'));
         dispatch(registry.get('redux-reducer', 'site').actions.setSite(item.name));
+        if (item.defaultLanguage !== current.language) {
+            dispatch(registry.get('redux-reducer', 'language').actions.setLanguage(item.defaultLanguage));
+        }
     }
 
     if (loading) {
@@ -30,7 +38,7 @@ const SiteSwitcher = () => {
     // + 1 for system sites as it is not in the list of knownSites
     if ((current.knownSitesList.length > 0) && ((current.knownSitesList.length + 1) !== data.jcr.result.nodes.length)) {
         refetch({
-            displayLanguage: 'en'
+            displayLanguage: current.uilang
         });
     }
 
@@ -41,7 +49,7 @@ const SiteSwitcher = () => {
             className={styles.siteSwitcher}
             data={data.jcr.result.nodes.filter(s => {
                 return s.hasPermission;
-            }).sort().map(s => ({label: s.displayName, value: s.path, name: s.name}))}
+            }).sort().map(s => ({label: s.displayName, value: s.path, name: s.name, defaultLanguage: s.defaultLanguage.value}))}
             onChange={(e, item) => handleOnChange(e, item)}
         />
     );
